6bc2a2d765ac3fd88cf8f72c71a4231b9e418c54
byoungjunjo <byoungjunjo@gmail.com>
2021-04-23 16:26:22 -0700

    3rd commit

--- scoreboard.py
+++ scoreboard.py
@@ -8,19 +8,25 @@ class Scoreboard(Turtle):
         super().__init__()
         self.hideturtle()
         self.penup()
+        with open('data.txt') as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.goto(0, 260)
         self.score = 0
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", move=False, align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", move=False, align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
-        self.clear()
         self.score += 1
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"GAME OVER.", move=False, align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.high_score < self.score:
+            self.high_score = self.score
+            with open('data.txt', mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
